2d0e5faf6926d3519ece652e9e85cccadd83f54f
1970luz <122659156+1970luz@users.noreply.github.com>
2023-01-19 22:33:58 -0600

    Add files via upload

---
+++ LoginUnisurl/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace LoginUnisurl
+{
+    partial class buttonEntrar
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxLogin = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelSaludo = new System.Windows.Forms.Label();
+            this.textBoxContrasena = new System.Windows.Forms.TextBox();
+            this.textBoxUsuario = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxLogin
+            // 
+            this.pictureBoxLogin.Image = global::LoginUnisurl.Properties.Resources.candadocerrado;
+            this.pictureBoxLogin.Location = new System.Drawing.Point(72, 92);
+            this.pictureBoxLogin.Name = "pictureBoxLogin";
+            this.pictureBoxLogin.Size = new System.Drawing.Size(128, 92);
+            this.pictureBoxLogin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxLogin.TabIndex = 0;
+            this.pictureBoxLogin.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(297, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "LOGIN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(276, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "USUARIO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(251, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "CONTRASEÑA";
+            // 
+            // labelSaludo
+            // 
+            this.labelSaludo.AutoSize = true;
+            this.labelSaludo.Location = new System.Drawing.Point(69, 274);
+            this.labelSaludo.Name = "labelSaludo";
+            this.labelSaludo.Size = new System.Drawing.Size(91, 13);
+            this.labelSaludo.TabIndex = 4;
+            this.labelSaludo.Text = "acceso al sistema";
+            // 
+            // textBoxContrasena
+            // 
+            this.textBoxContrasena.Location = new System.Drawing.Point(338, 163);
+            this.textBoxContrasena.Name = "textBoxContrasena";
+            this.textBoxContrasena.Size = new System.Drawing.Size(100, 20);
+            this.textBoxContrasena.TabIndex = 7;
+            // 
+            // textBoxUsuario
+            // 
+            this.textBoxUsuario.Location = new System.Drawing.Point(341, 125);
+            this.textBoxUsuario.Name = "textBoxUsuario";
+            this.textBoxUsuario.Size = new System.Drawing.Size(97, 20);
+            this.textBoxUsuario.TabIndex = 8;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(269, 261);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(95, 26);
+            this.buttonLogin.TabIndex = 9;
+            this.buttonLogin.Text = "ENTRAR";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // buttonSalir
+            // 
+            this.buttonSalir.Location = new System.Drawing.Point(417, 261);
+            this.buttonSalir.Name = "buttonSalir";
+            this.buttonSalir.Size = new System.Drawing.Size(95, 26);
+            this.buttonSalir.TabIndex = 10;
+            this.buttonSalir.Text = "SALIR";
+            this.buttonSalir.UseVisualStyleBackColor = true;
+            this.buttonSalir.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonEntrar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSalir);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.textBoxUsuario);
+            this.Controls.Add(this.textBoxContrasena);
+            this.Controls.Add(this.labelSaludo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxLogin);
+            this.Name = "buttonEntrar";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.buttonEntrar_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelSaludo;
+        private System.Windows.Forms.TextBox textBoxContrasena;
+        private System.Windows.Forms.TextBox textBoxUsuario;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button buttonSalir;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ LoginUnisurl/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginUnisurl
+{
+    public partial class buttonEntrar : System.Windows.Forms.Form
+    {
+        int contador = 0;
+        int intentosmaximos = 3;
+        string user ="luci";
+        string contrasena = "12345";
+
+
+        public buttonEntrar()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonEntrar_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            if (textBoxUsuario.Text == user & textBoxContrasena.Text == contrasena)
+            {
+                MessageBox.Show("Bienvenido");
+                    pictureBoxLogin.Image = Properties.Resources.candadoabuerto;
+                labelSaludo.Text = "Bienvenido al sistema";
+            }
+            else
+            {
+                contador = contador + 1;
+                if (contador>=3)
+                {
+                    MessageBox.Show("Has superado el maximo de intentos " + intentosmaximos);
+                    pictureBoxLogin.Image = Properties.Resources.candadocerrado;
+                    buttonLogin.Enabled = false;
+
+                }
+                else
+
+                {
+                    MessageBox.Show("Usuario o contraseña incorrecta,te quedan  " +( intentosmaximos- contador)+ " intentos.");
+                }
+            }
+
+                
+        }
+    }
+}
